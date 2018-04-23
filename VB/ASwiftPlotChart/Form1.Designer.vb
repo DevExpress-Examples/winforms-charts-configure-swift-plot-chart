e7ec984d8e7600b10995e156c8b8ab3d9d54ad06
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/ASwiftPlotChart/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿Imports DevExpress.XtraCharts
+Namespace ASwiftPlotChart
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim swiftPlotDiagram1 As New DevExpress.XtraCharts.SwiftPlotDiagram()
+            Dim series1 As New DevExpress.XtraCharts.Series()
+            Dim swiftPlotSeriesView1 As New DevExpress.XtraCharts.SwiftPlotSeriesView()
+            Dim sideBySideBarSeriesLabel1 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim chartTitle1 As New DevExpress.XtraCharts.ChartTitle()
+            Me.timer1 = New System.Windows.Forms.Timer(Me.components)
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(swiftPlotDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(series1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(swiftPlotSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' timer1
+            ' 
+            Me.timer1.Enabled = True
+            Me.timer1.Interval = 10
+            ' 
+            ' chartControl1
+            ' 
+            swiftPlotDiagram1.AxisX.DateTimeScaleOptions.MeasureUnit = DateTimeMeasureUnit.Millisecond
+            swiftPlotDiagram1.AxisX.Label.TextPattern = "{A:mm:ss}"
+            swiftPlotDiagram1.AxisX.WholeRange.AlwaysShowZeroLevel = True
+            swiftPlotDiagram1.AxisX.WholeRange.AutoSideMargins = True
+            swiftPlotDiagram1.AxisX.VisualRange.AutoSideMargins = True
+            swiftPlotDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            swiftPlotDiagram1.AxisY.WholeRange.AlwaysShowZeroLevel = True
+            swiftPlotDiagram1.AxisY.WholeRange.AutoSideMargins = True
+            swiftPlotDiagram1.AxisY.VisualRange.AutoSideMargins = True
+            swiftPlotDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            swiftPlotDiagram1.Margins.Left = 40
+            swiftPlotDiagram1.Margins.Right = 30
+            Me.chartControl1.Diagram = swiftPlotDiagram1
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            series1.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.DateTime
+            series1.LegendText = "Swift Plot Series"
+            series1.Name = "series1"
+            swiftPlotSeriesView1.Antialiasing = True
+            series1.View = swiftPlotSeriesView1
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
+            sideBySideBarSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel1
+            Me.chartControl1.Size = New System.Drawing.Size(459, 264)
+            Me.chartControl1.TabIndex = 0
+            chartTitle1.Text = "The Swift Plot"
+            Me.chartControl1.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() { chartTitle1})
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(459, 264)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(swiftPlotDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(swiftPlotSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(series1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
+
+        #End Region
+
+        Private WithEvents timer1 As System.Windows.Forms.Timer
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
+
+    End Class
+End Namespace
+
