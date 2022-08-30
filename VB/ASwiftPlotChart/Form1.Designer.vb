b910831764335b50c35a83613ab3b577e67eec6f
DevExpressExampleBot <fakeEmail>
2022-08-30 11:14:53 +0400

    Source auto update [skip ci]

--- VB/ASwiftPlotChart/Form1.Designer.vb
+++ VB/ASwiftPlotChart/Form1.Designer.vb
@@ -1,6 +1,9 @@
-﻿Imports DevExpress.XtraCharts
+Imports DevExpress.XtraCharts
+
 Namespace ASwiftPlotChart
-    Partial Public Class Form1
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -11,42 +14,43 @@ Namespace ASwiftPlotChart
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim swiftPlotDiagram1 As New DevExpress.XtraCharts.SwiftPlotDiagram()
-            Dim series1 As New DevExpress.XtraCharts.Series()
-            Dim swiftPlotSeriesView1 As New DevExpress.XtraCharts.SwiftPlotSeriesView()
-            Dim sideBySideBarSeriesLabel1 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
-            Dim chartTitle1 As New DevExpress.XtraCharts.ChartTitle()
+            Dim swiftPlotDiagram1 As DevExpress.XtraCharts.SwiftPlotDiagram = New DevExpress.XtraCharts.SwiftPlotDiagram()
+            Dim series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim swiftPlotSeriesView1 As DevExpress.XtraCharts.SwiftPlotSeriesView = New DevExpress.XtraCharts.SwiftPlotSeriesView()
+            Dim sideBySideBarSeriesLabel1 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim chartTitle1 As DevExpress.XtraCharts.ChartTitle = New DevExpress.XtraCharts.ChartTitle()
             Me.timer1 = New System.Windows.Forms.Timer(Me.components)
             Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(swiftPlotDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(swiftPlotSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((swiftPlotDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((swiftPlotSeriesView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' timer1
             ' 
             Me.timer1.Enabled = True
             Me.timer1.Interval = 10
+            AddHandler Me.timer1.Tick, New System.EventHandler(AddressOf Me.timer1_Tick)
             ' 
             ' chartControl1
             ' 
-            swiftPlotDiagram1.AxisX.DateTimeScaleOptions.MeasureUnit = DateTimeMeasureUnit.Millisecond
+            swiftPlotDiagram1.AxisX.DateTimeScaleOptions.MeasureUnit = DevExpress.XtraCharts.DateTimeMeasureUnit.Millisecond
             swiftPlotDiagram1.AxisX.Label.TextPattern = "{A:mm:ss}"
             swiftPlotDiagram1.AxisX.WholeRange.AlwaysShowZeroLevel = True
             swiftPlotDiagram1.AxisX.WholeRange.AutoSideMargins = True
@@ -60,7 +64,7 @@ Namespace ASwiftPlotChart
             swiftPlotDiagram1.Margins.Right = 30
             Me.chartControl1.Diagram = swiftPlotDiagram1
             Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False
+            Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.[False]
             Me.chartControl1.Location = New System.Drawing.Point(0, 0)
             Me.chartControl1.Name = "chartControl1"
             series1.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.DateTime
@@ -68,13 +72,13 @@ Namespace ASwiftPlotChart
             series1.Name = "series1"
             swiftPlotSeriesView1.Antialiasing = True
             series1.View = swiftPlotSeriesView1
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
-            sideBySideBarSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {series1}
+            sideBySideBarSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
             Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel1
             Me.chartControl1.Size = New System.Drawing.Size(459, 264)
             Me.chartControl1.TabIndex = 0
             chartTitle1.Text = "The Swift Plot"
-            Me.chartControl1.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() { chartTitle1})
+            Me.chartControl1.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() {chartTitle1})
             ' 
             ' Form1
             ' 
@@ -84,19 +88,17 @@ Namespace ASwiftPlotChart
             Me.Controls.Add(Me.chartControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(swiftPlotDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(swiftPlotSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((swiftPlotDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((swiftPlotSeriesView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
         End Sub
 
-        #End Region
+'#End Region
+        Private timer1 As System.Windows.Forms.Timer
 
-        Private WithEvents timer1 As System.Windows.Forms.Timer
         Private chartControl1 As DevExpress.XtraCharts.ChartControl
-
     End Class
 End Namespace
-
--- VB/ASwiftPlotChart/Form1.vb
+++ VB/ASwiftPlotChart/Form1.vb
@@ -1,27 +1,29 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
-' ...
 
+' ...
 Namespace ASwiftPlotChart
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Const interval As Integer = 20
-        Private random As New Random()
+        Const interval As Integer = 20
+
+        Private random As Random = New Random()
+
         Private TimeInterval As Integer = 10
+
         Private value1 As Double = 10.0
 
-        Private ReadOnly Property AxisXRange() As Range
+        Private ReadOnly Property AxisXRange As Range
             Get
                 Dim diagram As SwiftPlotDiagram = TryCast(chartControl1.Diagram, SwiftPlotDiagram)
-                If diagram IsNot Nothing Then
-                    Return diagram.AxisX.VisualRange
-                End If
+                If diagram IsNot Nothing Then Return diagram.AxisX.VisualRange
                 Return Nothing
             End Get
         End Property
@@ -34,38 +36,32 @@ Namespace ASwiftPlotChart
             value1 = CalculateNextValue(value1)
         End Sub
 
-        Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles timer1.Tick
+        Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs)
             Dim series1 As Series = chartControl1.Series(0)
-
-            If series1 Is Nothing Then
-                Return
-            End If
+            If series1 Is Nothing Then Return
             Dim argument As Date = Date.Now
-            Dim pointsToUpdate1(interval - 1) As SeriesPoint
+            Dim pointsToUpdate1 As SeriesPoint() = New SeriesPoint(19) {}
             For i As Integer = 0 To interval - 1
                 pointsToUpdate1(i) = New SeriesPoint(argument, value1)
                 argument = argument.AddMilliseconds(1)
                 UpdateValues()
-            Next i
+            Next
+
             Dim minDate As Date = argument.AddSeconds(-TimeInterval)
             Dim pointsToRemoveCount As Integer = 0
             For Each point As SeriesPoint In series1.Points
-                If point.DateTimeArgument < minDate Then
-                    pointsToRemoveCount += 1
-                End If
-            Next point
-            If pointsToRemoveCount < series1.Points.Count Then
-                pointsToRemoveCount -= 1
-            End If
+                If point.DateTimeArgument < minDate Then pointsToRemoveCount += 1
+            Next
+
+            If pointsToRemoveCount < series1.Points.Count Then pointsToRemoveCount -= 1
             series1.Points.AddRange(pointsToUpdate1)
             If pointsToRemoveCount > 0 Then
                 series1.Points.RemoveRange(0, pointsToRemoveCount)
             End If
+
             If AxisXRange IsNot Nothing Then
                 AxisXRange.SetMinMaxValues(minDate, argument)
             End If
-
         End Sub
-
     End Class
 End Namespace
